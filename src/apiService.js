e32618d9939702d5afd2f2e8ab4c8d85d935cd51
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-02-20 13:56:57 +0200

    create task

---
+++ src/apiService.js
@@ -0,0 +1,15 @@
+const KEY = '15302072-a81be31270c5e4995077a81d4';
+
+export default function serviseApi(searchValue, pageNumber, callBack) {
+  const baseUrl =`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchValue}&page=${pageNumber}&per_page=12&key=${KEY}`;
+  fetch(baseUrl).then(response => {
+    return response.json();
+ }) //делаем запрос фетчем по нашему URL - возвращает промис =>в then получаем ответ и 
+ // преобразовываем с помощью .json в обьект => в следующем then нам приходят уже данные
+ // и мы их прокидываем в callback => catch ловит ошибку если она произошла в момент получения данных!
+ .then(data =>{
+     callBack(data);
+ }).catch(error =>{
+     console.error(error);
+ });
+}
---
+++ src/index.js
@@ -0,0 +1,54 @@
+import './styles.css';
+import pixabayApiData from './apiService';
+import imageList from './template/image-list.hbs';
+import '../node_modules/material-design-icons/iconfont/material-icons.css';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basiclightbox.min.css';
+
+const ulListImage = document.querySelector('#image-list');
+const searchInput = document.querySelector('#input');
+let reSearch;
+const classBtn = document.querySelector('#classBtn');
+classBtn.addEventListener('click', onClickBtn);
+ulListImage.addEventListener('click', originImageShow);
+
+function parseData(data) {
+  const rezult = imageList(data);
+  //в наш шаблон кидаем наши hits
+  ulListImage.insertAdjacentHTML('beforeend', rezult);
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: 'smooth',
+  });
+}
+
+let currentPage = 0;
+
+function onClickBtn(event) {
+  if (reSearch !== searchInput.value) {
+    currentPage = 0;
+    console.log(`ENTRY`);
+  }
+  pixabayApiData(searchInput.value, ++currentPage, parseData);
+  reSearch = searchInput.value;
+  console.log(`Page number ---- ${currentPage}`);
+}
+
+function originImageShow(event) {
+  let clickImage = event.target;
+  // console.dir(clickImage);
+  if (clickImage.tagName === 'IMG') {
+    const instance = basicLightbox.create(
+      `<img src="${clickImage.dataset.origin}">`,
+    );
+    instance.show();
+  }
+}
+// instance.close();
+
+const btnToTop = document.querySelector('#fix-button');
+btnToTop.addEventListener('click', backToTop);
+
+function backToTop(event) {
+  window.scrollTo(pageXOffset, 0);
+}
